843478a26996ada83aa5a6137b21cbaeec9a3234
21f1002045 <132224922+21f1002045@users.noreply.github.com>
2023-05-01 00:06:23 +0530

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+export GITHUB_USERNAME=username of github
+export GITHUB_REPOSITERY=se2001-roll_no
+
+cd ~/se2001/assignment_4
+vi script.sh(inside this (for input bi)
+
+#!/bin/bash
+export GITHUB_USERNAME="username of github"
+export GITHUB_REPOSITERY="se2001-roll_no"
